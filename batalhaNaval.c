9998ee6f94b1454ec30b00a941bd153b34489c0b
L.J/ Leonam James <107443538+RaphManDey@users.noreply.github.com>
2025-04-15 15:05:39 +0000

    Removi 2 comentários e adicionei uns comentários 2 Vetores e 1 matriz loop for if e else printf e variáveis

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 // Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
